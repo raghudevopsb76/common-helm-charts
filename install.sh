f9bc2673947d7054f630818b361c7eedb84388cf
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2024-02-13 21:10:42 +0530

    RoboShop TF Code

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ helm repo update
 helm upgrade -i  nginx-ingress ingress-nginx/ingress-nginx -f nginx-ingress/values.yaml
 
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl apply -f argocd/ingress.yaml
+#kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -f argocd/argocd.yaml
 
 
 #
