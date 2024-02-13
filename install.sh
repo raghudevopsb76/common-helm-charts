5d037f263d53794a54d69b386683abfda86b27c5
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-02-13 06:25:29 +0530

    Kubernetes code

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
-helm install nginx-ingress ingress-nginx/ingress-nginx -f nginx-ingress/values.yaml
+helm upgrade -i  nginx-ingress ingress-nginx/ingress-nginx -f nginx-ingress/values.yaml
 
